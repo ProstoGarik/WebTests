270e8f11ca864686212ce61edd3647ba1337bff2
Garik <bbanan570@gmail.com>
2025-01-20 20:29:46 +0300

    commit

--- WebPage/using/footer-loader.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    fetch('/using/footer.html')
-        .then(response => response.text())
-        .then(html => {
-            document.body.insertAdjacentHTML('beforeend', html);
-        })
-        .catch(err => console.error('Ошибка загрузки футера:', err));
-});
